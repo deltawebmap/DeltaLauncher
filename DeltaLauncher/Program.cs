15464d9e6c2b5257028b911d12a11f77cc5b9faf
Roman Port <34401219+Roman-Port@users.noreply.github.com>
2019-08-14 18:00:52 -0500

    Add project files.

---
+++ DeltaLauncher/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DeltaLauncher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
